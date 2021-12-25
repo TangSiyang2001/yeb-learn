8e0a56a26894918efd3af4edd1047630c0542e58
tsy <984337688@qq.com>
2021-12-25 09:50:52 +0800

    add Dateconverter

--- yeb-backend/yeb-server/src/main/java/com/tsy/yebserver/common/convertor/DateConvertor.java
+++ yeb-backend/yeb-server/src/main/java/com/tsy/yebserver/common/convertor/DateConvertor.java
@@ -1,8 +1,29 @@
 package com.tsy.yebserver.common.convertor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
 /**
+ * 日期转换类
+ *
  * @author Steven.T
  * @date 2021/12/25
  */
-public class DateConvertor {
+@Slf4j
+@Component
+public class DateConvertor implements Converter<String, LocalDate> {
+
+    @Override
+    public LocalDate convert(String source) {
+        try {
+            return LocalDate.parse(source);
+        } catch (Exception exception) {
+            log.error("日期转换错误:{}",exception.getMessage());
+            return null;
+        }
+    }
+
 }
