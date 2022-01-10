88c1dcc3919f1fb64c2b335576f981fd86fab282
tsy <984337688@qq.com>
2022-01-10 11:15:59 +0800

    太添加工资账套基本功能，更改jwt过期时间

--- yeb-backend/yeb-server/src/main/java/com/tsy/yebserver/controller/SalaryController.java
+++ yeb-backend/yeb-server/src/main/java/com/tsy/yebserver/controller/SalaryController.java
@@ -1,8 +1,13 @@
 package com.tsy.yebserver.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tsy.yebserver.dao.entity.Salary;
+import com.tsy.yebserver.service.ISalaryService;
+import com.tsy.yebserver.vo.Result;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,4 +21,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/salary")
 public class SalaryController {
 
+    @Resource
+    private ISalaryService salaryService;
+
+    @GetMapping("/sob")
+    public Result listSalary(){
+        return Result.success(salaryService.list());
+    }
+
+    @PostMapping("/sob")
+    public Result addSalary(@RequestBody Salary salary){
+        salary.setCreateDate(LocalDateTime.now());
+        return salaryService.save(salary) ? Result.success(null) : Result.fail(Result.CodeMsg.OPERATION_FAILED);
+    }
+
+    @PutMapping("/sob")
+    public Result updateSalary(@RequestBody Salary salary){
+        return salaryService.updateById(salary) ? Result.success(null) : Result.fail(Result.CodeMsg.OPERATION_FAILED);
+    }
+
+    @DeleteMapping("/sob/{id}")
+    public Result deleteSalary(@PathVariable Integer id){
+        return salaryService.removeById(id) ? Result.success(null) : Result.fail(Result.CodeMsg.OPERATION_FAILED);
+    }
 }
